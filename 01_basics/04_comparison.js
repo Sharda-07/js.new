5ca22c315a8276be158c92710e3a8d899fb5bae7
Sharda Meghwal <162364648+Sharda-07@users.noreply.github.com>
2025-06-12 14:10:10 +0000

    comparison and stack/heap

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -7,6 +7,28 @@ console.log(null > 0);
 console.log(null == 0);
 console.log(null >= 0);
 
+//****** Stack("primitive") and Heap("Non primitive")
+
+let myYoutubename = "shardamdotcom"
+
+let anothername = myYoutubename
+anothername = "sanudotcom"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userone={
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+let userTwo = userone
+
+userTwo.email = "anand@google.com"
+
+console.log(userone.email);
+console.log(userTwo.email);
+
+
 
 
 
