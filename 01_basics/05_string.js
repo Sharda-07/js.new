acc11a11e329daf090f16e79bec4da1bceb3e396
Sharda Meghwal <162364648+Sharda-07@users.noreply.github.com>
2025-06-18 17:58:07 +0000

    strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,14 @@
+const name = "sharda"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+// this is widely not used in modern era and is also outdated.
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gamename = new String('sharda')
+console.log(gamename[4]);
+console.log(gamename.__proto__);
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+console.log(gamename.charAt(5));
