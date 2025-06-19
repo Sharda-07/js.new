c4336e81c8ebd09a25170b3c7b3188c4b878efa0
Sharda Meghwal <162364648+Sharda-07@users.noreply.github.com>
2025-06-19 13:59:46 +0000

    01arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,13 @@
+//arrays
+
+const myarr =[0,1,2,3,4,5]
+console.log(myarr[3]);
+
+// Array methods 
+myarr.push(6)
+myarr.push(7)
+myarr.pop()
+
+//myarr.unshift(9)  // array ke sabse aage number aa jayega
+myarr.shift(20)
+console.log(myarr.includes(3));
