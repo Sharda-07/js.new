6876ed6b3a98a7108a7a51ad192f2b9e13384c2f
Sharda Meghwal <162364648+Sharda-07@users.noreply.github.com>
2025-06-19 06:34:55 +0000

    numMath

---
+++ 01_basics/06_NumMaths.js
@@ -0,0 +1,14 @@
+const score = 199
+console.log(score);
+
+const balance = new Number (100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+// +++++++ MATHS +++++++
+console.log(Math.abs(-8));
+console.log(Math.round(4.5));
+console.log(Math.ceil(3.3));
+console.log(Math.floor(4.9));
